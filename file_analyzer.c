828c0585c1ea4f57cb5ff15a3e8f47bbc29f5a19
davidebaroffio-polimi <davide.baroffio@mail.polimi.it>
2025-04-25 17:23:28 +0200

    Local count of the result

--- file_analyzer.c
+++ file_analyzer.c
@@ -5,16 +5,19 @@
 
 long res=0;
 
-void count_lines(FILE *fd, int begin, int size) {
+int count_lines(FILE *fd, int begin, int size) {
+  int cnt = 0;
   fseek(fd, begin, SEEK_SET);
 
   for (int i=0; i < size; i++) {
     char c = fgetc(fd);
-    if (c == '\n') res++;
+    if (c == '\n') cnt++;
   }
+  return cnt;
 }
 
 int main() {
+
   FILE *fd = fopen("file.txt", "r");
   fseek(fd, 0, SEEK_END); // seek to end of file
   long size = ftell(fd); // get current file pointer
@@ -23,8 +26,8 @@ int main() {
   long chunk_size = size / NUM_CHUNKS;
   long remainder = size % NUM_CHUNKS;
   for (int i=0; i<NUM_CHUNKS-1; i++) {
-    count_lines(fd, i*chunk_size, chunk_size);
+    res+=count_lines(fd, i*chunk_size, chunk_size);
   }
-  count_lines(fd, (NUM_CHUNKS-1)*chunk_size, chunk_size + remainder);
+  res+=count_lines(fd, (NUM_CHUNKS-1)*chunk_size, chunk_size + remainder);
   printf("There are %ld lines\n", res);
 }
